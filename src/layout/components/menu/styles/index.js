586e179c634abd1b9bc5f0ea26729e3557caf370
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-11-22 17:59:04 -0300

    adiciona a tag logout

--- src/layout/components/menu/styles/index.js
+++ src/layout/components/menu/styles/index.js
@@ -1,21 +1,21 @@
-import { List, styled } from "@mui/material";
+import { Box, List, Stack, styled } from "@mui/material";
 import { Root } from "../../../../components/Global/Root/root_styles";
 export const LateralMenu = styled('div')(({ theme, isSmallScreen }) => ({
     position: isSmallScreen && 'fixed',
-    top:0,
-    left:0,
-    zIndex: 99999, 
+    top: 0,
+    left: 0,
+    zIndex: 99999,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'flex-start',
     flexDirection: 'column',
-    width: !isSmallScreen&&'35%',
+    width: !isSmallScreen && '35%',
     width: isSmallScreen && '90%',
     height: '100%',
     padding: '20px',
     backgroundColor: Root.white,
     color: Root.gray,
-    borderRight: !isSmallScreen &&Root.border,
+    borderRight: !isSmallScreen && Root.border,
     boxSizing: "border-box",
     transition: 'all .3s ease-in-out',
 }));
@@ -24,4 +24,24 @@ export const MinhaLista = styled(List)(() => ({
     height: '100%',
     width: '90%',
     transition: 'all 0.5s ease-in-out',
+}))
+export const Logout = styled(Box)(() => ({
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '12px',
+    overflow: 'hidden',
+    height: 'auto',
+    padding: '5px',
+    width: '100%',
+    maxWidth: '100%',
+    transition: 'all 0.5s ease-in-out',
+    boxShadow: Root.boxShadow,
+    '&:hover': {
+        backgroundColor: Root.color_button_secondary,
+        color: Root.color_default,
+        cursor: 'pointer',
+    },
+
 }))
