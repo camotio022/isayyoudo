39f53904323a0b9988938ab94e0c296fcc49029d
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-11-22 16:01:47 -0300

    Mudando as cores dos botões

--- src/auth/Login/styles/index.js
+++ src/auth/Login/styles/index.js
@@ -34,7 +34,7 @@ export const MiniButton = styled(Typography)(({ theme }) => ({
     alignItems: 'center',
     justifyContent: 'center',
     cursor: 'pointer',
-    color: '#6a5acd',
+    color: Root.color_default,
     width: "100%",
     marginTop: 10,
     mb: 2,
@@ -44,4 +44,5 @@ export const MiniButton = styled(Typography)(({ theme }) => ({
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
     textDecoration: 'none',
+    minHeight: '2rem',
 }));
