03e0499dddcc59994f3249b708d2b42104ba8224
camotio02 <temotio.bernardo@snowmanlabs.com>
2023-12-08 23:35:51 -0300

    aditando altura do logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,13 +1,11 @@
-import { Subject } from "@mui/icons-material"
 import { Stack } from "@mui/material"
-import { grey, orange, purple } from "@mui/material/colors"
-import { Fragment } from "react"
-import { Root } from "../Global/Root/root_styles"
-import Logo from './img/logo_default.png'
+import Logo from './img/teste.png'
 export const LogoPage = () => {
     return (
         <Stack sx={{ mb: 3 }}>
-            <img src={Logo} alt="" />
+            <img style={{
+                height: '7rem'
+            }} src={Logo} alt="" />
         </Stack>
     )
 }
