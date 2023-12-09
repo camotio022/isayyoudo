973f777d608d81ced82b5ab9261712de3ba8d91f
camotio02 <temotio.bernardo@snowmanlabs.com>
2023-12-08 23:36:40 -0300

    estilizando o overflow do card das tarefas

--- src/components/cardTask/styles/index.js
+++ src/components/cardTask/styles/index.js
@@ -6,12 +6,29 @@ export const TabsMain = styled(Box)(({ theme, border }) => ({
     minHeight: '5%',
     height: 'auto',
     paddingBlock: '1rem',
-    borderRadius: "4px",
+    borderRadius: '4px',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'flex-start',
     backgroundColor: Root.white,
     gap: 10,
     boxShadow: Root.boxShadowTask,
+    overflowX: 'auto',
+    overflowY: 'hidden',
 
-}))
+    '&::-webkit-scrollbar': {
+        width: '5px',
+        height: '3px'
+    },
+
+    '&::-webkit-scrollbar-thumb': {
+        backgroundColor: '#f1f1f1',
+        borderRadius: '6px',
+    },
+
+    '&::-webkit-scrollbar-track': {
+        backgroundColor: '#f1f1f1',
+    },
+    scrollbarWidth: 'thin',
+    scrollbarColor: '#888 #f1f1f1',
+}));
