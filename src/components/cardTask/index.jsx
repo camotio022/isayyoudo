058694c9004dc3145ae5ed9b6574509bb59fb8f0
Camotio02 <temotio.bernardo@snowmanlabs.com>
2024-01-15 16:18:08 -0300

    fixando o details task component

--- src/components/cardTask/index.jsx
+++ src/components/cardTask/index.jsx
@@ -7,6 +7,7 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import { taskService } from '../../api/tasks/addTask.js'
 import { useEffect, useState } from 'react'
 import { DescriptionsAndComments } from '../DescriptionsAndComments/index.jsx'
+import { TaskDetailed } from '../TaskDetailed/index.jsx'
 
 export const TaskCard = ({
     name,
@@ -23,7 +24,6 @@ export const TaskCard = ({
     setOpenMoreInfo,
     openMoreInfo,
     width,
-    backgroundColor,
     typeShowTask,
     taskId,
     tasks
@@ -68,6 +68,13 @@ export const TaskCard = ({
         <T.TabsMain width={width} backgroundColor={false} onDoubleClick={deleteTask}>
             <Assignment sx={{ margin: '1rem', color: colorStatus }} />
             {open && <DescriptionsAndComments setOPen={setOPen} />}
+            <TaskDetailed
+                taskId={taskId}
+                open={openMoreInfo}
+                setOpenMoreInfo={setOpenMoreInfo}
+                handleClick={handleClick}
+                task={task}
+            />
             <T.CircleStateTask color={colorStatus} sx={{
                 position: 'absolute',
                 ml: 4,
