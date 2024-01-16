076b8e01b5b069d5b5ab7b52722231c0031c8e8e
Camotio02 <temotio.bernardo@snowmanlabs.com>
2024-01-16 17:58:52 -0300

    alterando a interface do cardMobile

---
+++ src/components/CardTaskMobile/index.js
@@ -0,0 +1,38 @@
+import { Box, MenuItem, Stack, styled,Typography } from "@mui/material";
+import { Root } from "../Global/Root/root_styles";
+
+export const Menus = styled(Box)(({ theme }) => ({
+    height: '1.5rem',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+}));
+export const MuiTypography = styled(Typography)(({ theme, fontWeight, color }) => ({
+    fontSize: '14px',
+    fontWeight: fontWeight? fontWeight: 600,
+    color: color? color: Root.gray
+}));
+export const MenuItems = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: '2.8rem',
+}));
+export const MenuStack = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: 180,
+    height: '100%',
+    gap: 0.5
+}));
+export const MenuRadiusItem = styled(Box)(({ theme, backgroundColor }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: backgroundColor?'rgba(0, 0, 0, 0.1)': Root.white,
+    height: '1.6rem',
+    color: Root.black,
+    borderRadius: '2.3rem',
+    paddingInline: 8
+}));
--- src/components/CardTaskMobile/index.jsx
+++ src/components/CardTaskMobile/index.jsx
@@ -1,10 +1,28 @@
-import { Avatar, Box, Button, Card, CardActions, CardContent, Grid, Paper, Stack, Tooltip, Typography } from "@mui/material";
+import { Avatar, Box, Button, Card, CardActions, CardContent, Divider, Grid, MenuItem, Paper, Stack, Tooltip, Typography } from "@mui/material";
 import { Root } from "../Global/Root/root_styles";
 import { avatars } from "./avatars";
-import { taskStatusBgcolor } from "../../pages/createTasks/quirys/taskStatus";
+import *  as Tag from './index.js'
+import { taskStatusBgcolor, taskStatusColors } from "../../pages/createTasks/quirys/taskStatus";
 import { DescriptionsAndComments } from "../DescriptionsAndComments";
 import { useState } from "react";
 import { TaskDetailed } from "../TaskDetailed";
+import { MyButton } from "../Global/Styles/styles.jsx";
+import { Circle, Collections, PlaylistAddCheck } from "@mui/icons-material";
+import { BullPoint } from "../Bull/index.jsx";
+const SectionsItems = ({
+    one, two, backgroundColor, color, bull
+}) => {
+    return (
+        <Tag.MenuRadiusItem backgroundColor={backgroundColor}>
+            <Tag.MuiTypography>
+                {one}
+            </Tag.MuiTypography>
+            <Tag.MuiTypography sx={{ color: color ? color : Root.black }} >
+                {bull && <Circle sx={{ fontSize: 8, mr: 1 }} />}{two}
+            </Tag.MuiTypography>
+        </Tag.MenuRadiusItem>
+    )
+}
 export const CardTaskMobile = (
     {
         tasks,
@@ -36,6 +54,9 @@ export const CardTaskMobile = (
         setTask(task)
         setOpenMoreInfo(!openMoreInfo)
     }
+    const checkKey = (key) => {
+        return key ? key : 'Empety'
+    }
     const calculateExecutionDays = (start, delivery) => {
         const startDate = new Date(start);
         const deliveryDate = new Date(delivery);
@@ -49,97 +70,94 @@ export const CardTaskMobile = (
     const [open, setOPen] = useState(false)
     return (
         <>
-            <Tooltip title={'Click para ver mais informações dessa tarefa!!'} >
-                <Grid item xs={8} sx={{
-                    width: mobile ? '90%' : 278,
-                    height: mobile ? 138 : 200,
-                    cursor: 'pointer',
-                    boxShadow: Root.boxShadow,
-                }}>
-                    <Card variant="outlined" sx={{
-                        height: '100%',
-                        borderRadius: 'none',
-                        border: 'none',
-                    }}>
-                        <Stack sx={{
-                            display: 'flex',
-                            alignItems: 'space-beetwen',
-                            justifyContent: 'flex-start',
-                            flexDirection: 'row',
-                            width: '95%'
-                        }}>
-                            <Box ml={1}>
-                                <Typography
-                                    onClick={() => (
-                                        setOPen(true)
-                                    )}
-                                    sx={threePoints}
-                                    variant="div"
-                                    component="div"
+            <Grid item xs={8} sx={{
+                width: mobile ? '90%' : 300,
+                height: mobile ? 270 : 270,
+                cursor: 'pointer',
+                boxShadow: Root.boxShadow,
+                padding: 1.2
+            }}>
+                <Tag.Menus>
+                    <Tag.MuiTypography>
+                        {checkKey(task.startDate)}
+                    </Tag.MuiTypography>
+                    <Tag.MuiTypography>
+                        {checkKey(task.assigner)}
+                    </Tag.MuiTypography>
+                </Tag.Menus>
+                <Divider sx={{ bgcolor: color ? color : emptyBgcolor }} />
+                <Tooltip onClick={handleClick} title={'Click para ver mais informações dessa tarefa!!'} >
+                    <Tag.MenuItems >
+                        <Box>
+                            <Tag.MenuStack>
+                                <PlaylistAddCheck sx={{ color: Root.black }} />
+                                <Tag.MuiTypography
+                                    fontWeight={"900"}
+                                    color={Root.black}
                                 >
-                                    {task.title ? task?.title : 'Empty'}
-                                </Typography>
-                                <Typography sx={{ textTransform: 'uppercase' }} mt={1} color={'text.secondary'}>
-                                    {task.typeCollection ? task.typeCollection : 'Empty'}
-                                </Typography>
-                            </Box>
-                            {calculateExecutionDays(task.startDate, task.deliveryDate) &&
-                                <Stack
-                                    sx={{
-                                        width: '160px',
-                                        height: mobile ? '50px' : '120px',
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center',
-                                        color:
-                                            Root.color_button,
-                                        fontSize: '1.5rem',
-                                        fontWeight: 600
-                                    }}
-                                    variant="div"
-                                    component="div"
-                                >
-                                    <Box>
-                                        {calculateExecutionDays(task.startDate, task.deliveryDate)}
-                                    </Box>
-                                    <Box sx={{
-                                        fontSize: '0.8rem',
-                                        fontWeight: 600
-                                    }}>
-                                        Days Delivery
-                                    </Box>
-                                </Stack>
+                                    {checkKey(task.typeCollection)}
+                                </Tag.MuiTypography>
+                            </Tag.MenuStack>
+                        </Box>
+                        <MyButton
+                            colorsTask={
+                                color ? color : taskStatusColors.empty
                             }
-                        </Stack>
-                        <CardActions sx={{ flexDirection: 'row' }}>
-                            <Button
-                                onClick={handleClick}
-                                sx={{
-                                    bgcolor: backgroundColor ?
-                                        backgroundColor :
-                                        empty
-                                }} size="small">
-                                {task.taskStatus ? task.taskStatus : 'Empty'}
-                            </Button>
-                            <Stack direction="row" spacing={-1.5} alignItems="center">
-                                {firstThreeItems.map((avatar) => (
-                                    <>
-                                        <Avatar sx={{ width: 30, height: 30 }} key={avatar.id} src={avatar.src} alt={`Avatar ${avatar.id}`} />
-                                    </>
-                                ))}
-                            </Stack>
-                        </CardActions>
-                    </Card>
-                    {open && <DescriptionsAndComments setOPen={setOPen} />}
-                    <TaskDetailed
-                        taskId={task.taskId}
-                        open={openMoreInfo}
-                        setOpenMoreInfo={setOpenMoreInfo}
-                        handleClick={handleClick}
-                        task={task}
+                            backgroundColor={
+                                backgroundColor ? backgroundColor : taskStatusBgcolor.empty}
+                        >
+                            {checkKey(task.taskStatus)}
+                        </MyButton>
+                    </Tag.MenuItems>
+                </Tooltip>
+                <Tooltip onClick={() => (
+                    setOPen(true)
+                )} title={'Click para ver mais informações sobre comentários e tarefas!!'} >
+                    <Tag.MuiTypography sx={{
+                        height: '3rem',
+                        overflowY: 'auto',
+                        overflowX: 'hidden',
+                    }}>
+                        {checkKey(task.title)}
+                    </Tag.MuiTypography>
+                    <SectionsItems
+                        backgroundColor={true}
+                        one={'Start Date'}
+                        two={checkKey(task.startDate)}
                     />
-                </Grid>
-            </Tooltip>
+                    <SectionsItems
+                        backgroundColor={false}
+                        one={'Delivery Date'}
+                        two={checkKey(task.deliveryDate)}
+                    />
+                    <SectionsItems
+                        backgroundColor={true}
+                        one={'Assigner'}
+                        two={checkKey(task.assigner)}
+                    />
+                    <SectionsItems
+                        backgroundColor={false}
+                        one={'Assigned'}
+                        two={checkKey(task.assigned)}
+                    />
+                    <SectionsItems
+                        bull={true}
+                        backgroundColor={true}
+                        one={'Delivered Status'}
+                        two={checkKey(task.taskStatus)}
+                        color={color ? color : taskStatusColors.empty}
+                    />
+                </Tooltip>
+                {open && <DescriptionsAndComments setOPen={setOPen} />}
+                <TaskDetailed
+                    taskId={task.taskId}
+                    open={openMoreInfo}
+                    setOpenMoreInfo={setOpenMoreInfo}
+                    handleClick={handleClick}
+                    task={task}
+                />
+            </Grid>
+
         </>
     )
 }
