bfb3e9c851fb40d983326bee738afd03f745f38a
camotio022 <123275976+camotio022@users.noreply.github.com>
2024-01-24 17:57:50 -0300

    Remove os espaços não usados

--- src/pages/subpages/archived/index.jsx
+++ src/pages/subpages/archived/index.jsx
@@ -82,7 +82,6 @@ export const ArchivedTasks = () => {
                     )
                 }) : (
                     <NoTasksFromThisState routeTasks={'Archived'} />
-
                 )}
         </CardTaskMacthes>
 
