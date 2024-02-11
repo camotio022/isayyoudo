2ee383140af6a2870410416cf9816c02fbc20853
camotio022 <123275976+camotio022@users.noreply.github.com>
2024-02-10 23:58:00 -0300

    Filtrando os links no mobile

--- src/layout/components/links/links_a.jsx
+++ src/layout/components/links/links_a.jsx
@@ -8,6 +8,7 @@ import { Root } from '../../../components/Global/Root/root_styles';
 import { NotificationCounter } from '../../../components/Global/Styles/styles';
 import { useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
+
 export const Links_a = ({
   name,
   link,
@@ -16,27 +17,41 @@ export const Links_a = ({
   closeMenuLinks,
 }) => {
   const location = useLocation();
-  const isRoute = location.pathname === link
+  const isRoute = location.pathname === link;
+
   return (
-    <>
-      <Link to={link} >
-        <ListItemButton onClick={closeMenuLinks} style={{
+    <Link to={link} style={{ textDecoration: 'none', color: 'inherit' }} onClick={closeMenuLinks}>
+      <ListItemButton
+        sx={{
+          color: Root.gray,
           transition: 'all .2s ease-in-out',
-          borderLeft: isRoute && `2px solid${Root.color_button}`
+          borderLeft: isRoute && `5px solid ${Root.color_button}`,
+          borderRight: isRoute && Root.border,
+          boxShadow: isRoute && Root.boxS,
+          ':hover': {
+            borderLeft: `2px solid ${Root.color_button}`,
+          }
         }}
-          sx={{ color: Root.gray }} >
-          <ListItemIcon sx={{ color: Root.gray }}>{icon}</ListItemIcon>
-          <ListItemText primary={name} />
-          {canShowAlert &&
-            <Badge
-              badgeContent={
-                <NotificationCounter>
-                  {10}
-                </NotificationCounter>
-              }
-            />}
-        </ListItemButton>
-      </Link>
-    </>
+      >
+        <ListItemIcon sx={{
+          color: isRoute ?
+            Root.color_button :
+            Root.gray
+        }}>{icon}</ListItemIcon>
+        <ListItemText sx={isRoute && {
+          color: Root.color_button,
+        }} primary={name} />
+        {canShowAlert && (
+          <Badge
+            badgeContent={
+              <NotificationCounter>
+                {10} {/* Conteúdo do contador de notificações */}
+              </NotificationCounter>
+            }
+          />
+        )}
+      </ListItemButton>
+    </Link>
+
   );
 };
