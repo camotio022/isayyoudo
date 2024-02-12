5afdb5508c19c9709084f5589307ceba610a0f66
camotio022 <123275976+camotio022@users.noreply.github.com>
2024-02-12 00:49:37 -0300

    criando notificações

--- src/components/DrawerNotification/index.jsx
+++ src/components/DrawerNotification/index.jsx
@@ -1,60 +1,114 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import { useState } from 'react';
+import { Avatar, Box, CardContent, CardHeader, Divider, MenuItem, Stack, Typography } from '@mui/material';
+import { AdUnits, AppShortcut, ChatBubble, Comment, EventNote, Message, MoreHoriz, Notifications, Settings, SystemUpdate } from '@mui/icons-material';
+import { Root } from '../Global/Root/root_styles';
+import * as Tag from './styles/index.js'
+import { notificationsMasks } from './mask/index.js';
+
 
 export const DrawerNotification = ({
     open,
     toggleDrawer,
 }) => {
+    const notificationIcons = {
+        message: <Message fontSize='small' />,
+        order: <AdUnits />,
+        system_update: <SystemUpdate />,
+        event_reminder: <EventNote />,
+        special_offer: <AppShortcut />,
+    };
+    const texte = 'Notifications'
     return (
         <div>
-            <Drawer
+            <Tag.ContainerDrawer
                 anchor="right"
                 open={open}
                 onClose={toggleDrawer(false)}
             >
-                <div
+                <Tag.ContainerNotifications
                     role="presentation"
                     onClick={toggleDrawer(false)}
                     onKeyDown={toggleDrawer(false)}
                 >
-                    <List >
-                        {['Inbox', 'Starred', 'Send email mais item nesse', 'Drafts'].map((text, index) => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                    </ListItemIcon>
-                                    <ListItemText primary={text} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
-                    <Divider />
-                    <List>
-                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                    </ListItemIcon>
-                                    <ListItemText primary={text} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
-                </div>
-            </Drawer>
+                    <Tag.AppBarNotifications >
+                        <Tag.TitleTypho ml={2} canUpper={true}>
+                            {texte}
+                        </Tag.TitleTypho>
+                        <Tag.SettingsIcon sx={{mr:2}}/>
+                    </Tag.AppBarNotifications>
+                </Tag.ContainerNotifications>
+
+
+                {notificationsMasks.map((notification, index) => {
+                    return (
+                        <Tag.NotificationComponent
+                            key={index}
+                            sx={{ bgcolor: notification.isRead && Root.color_app_bar }}
+                            first={
+                                index === 0} last={
+                                    index
+                                    ===
+                                    notificationsMasks.length - 1
+                                }
+                        >
+                            <Tag.WrapperNotification>
+                                {notification.type !== 'message' && <Avatar sx={{
+                                    color: Root.color_button,
+                                    border: `1px solid ${Root.color_app_bar}`,
+                                    bgcolor: Root.color_app_bar,
+                                    boxShadow: Root.boxS
+                                }}>
+                                    {notificationIcons[notification.type]}
+                                </Avatar>}
+                                {notification.type === 'message' && <Tag.AvatarCommentNotification >
+                                    <Tag.AvatarCommentActionFlutuente>
+                                        <ChatBubble fontSize='small' sx={{ color: Root.color_button }} />
+                                    </Tag.AvatarCommentActionFlutuente>
+                                    <Stack sx={{ color: Root.cyan }}>
+                                        {notificationIcons[notification.type]}
+                                    </Stack>
+                                </Tag.AvatarCommentNotification>}
+                                <span>
+                                    <Stack ml={2}>
+                                        <Tag.TitleTypho>
+                                            {notification.title}
+                                        </Tag.TitleTypho>
+                                        <Tag.ContentNotifications variant='subtitle1' sx={{
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            flexWrap: 'wrap',
+                                            minHeight: '1rem',
+                                            color: notification.isRead ? Root.color_button : Root.gray_desfius
+                                        }}>
+                                            {notification.content}
+                                        </Tag.ContentNotifications>
+
+                                    </Stack>
+                                    <Typography variant='subtitle2' ml={2} sx={{
+                                        color: notification.isRead ? Root.color_button : Root.gray_desfius
+                                    }}>
+                                        {'Janeiro de 2023'}
+                                    </Typography>
+                                </span>
+                                <Stack sx={{
+                                    position: 'absolute',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    gap: 2,
+                                    color: Root.gray_desfius,
+                                    right: 0,
+                                    border: `1px solid ${Root.color_app_bar}`
+                                }}>
+                                    <MoreHoriz />
+                                </Stack>
+                            </Tag.WrapperNotification>
+                        </Tag.NotificationComponent>
+                    )
+                })}
+            </Tag.ContainerDrawer>
         </div>
     );
 };
